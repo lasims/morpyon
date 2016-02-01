97550bf1b97e3915072ce2fd913d9a0a44a8fd7b
Simon <sims38930@gmail.com>
2016-02-01 15:08:54 +0100

    on creer une sortie propre

--- morpyon.py
+++ morpyon.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
+
+from sys import exit
 import pygame
+
 pygame.display.set_mode((640, 400))
 
 while 1:
-    pass
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        exit(0)
